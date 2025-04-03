3387008ebb84fae4e78720aa844bc91fabf3d907
Gopal Khadka <khadkagopal.mail@gmail.com>
2025-04-03 20:17:44 +0545

    fix: Make navbar responsive

--- src/components/navigation-menu/navbar.tsx
+++ src/components/navigation-menu/navbar.tsx
@@ -28,22 +28,19 @@ const navItems = [
 const Navbar = () => {
   return (
     <header>
-      <nav className="fixed top-6 left-6 z-50 w-[394px] h-[84px] p-5 bg-white rounded-lg shadow-md flex justify-between items-center">
-        <span
-          id="logo"
-          className="font-semibold text-[18px] leading-7 tracking-normal"
-        >
-          Tilak Joshi
-        </span>
-        <div id="nav-items" className="hidden tablet:flex">
-          {navItems.map((navItem, idx) => (
-            <div key={idx}>
-              <a href={navItem.link}>{navItem.text}</a>
-            </div>
+      <nav className="fixed top-0 inset-x-0 mx-auto w-[calc(100%-48px)] tablet:max-w-[1024px] mt-6 h-21 px-5 bg-background z-50 rounded-lg shadow-md flex justify-between items-center">
+        <span className="font-semibold text-lg">Tilak Joshi</span>
+
+        <div className="hidden tablet:flex gap-6">
+          {navItems.map((item, idx) => (
+            <a key={idx} href={item.link} className="hover:text-primary">
+              {item.text}
+            </a>
           ))}
         </div>
-        <div id="nav-menu" className="flex gap-5 items-center">
-          <Button className="bg-primary rounded-[6px] py-2.5 px-5 font-bold text-lg leading-6 gap-2.5 h-11">
+
+        <div className="flex gap-5 items-center">
+          <Button className="bg-primary hidden mobile:inline-flex">
             Let's connect
           </Button>
           <Menu className="block tablet:hidden" />
